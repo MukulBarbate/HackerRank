edcbaacf2c3a802d584c2bfefa7c5fff52290fe2
MukulBarbate <72039148+MukulBarbate@users.noreply.github.com>
2021-06-16 15:12:04 +0530

    Create alternatingCharacters.py
    You are given a string containing characters A and B only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
    
    Your task is to find the minimum number of required deletions.
    
    Example
    
    Remove an  at positions  and  to make  in  deletions.
    
    Function Description
    
    Complete the alternatingCharacters function in the editor below.
    
    alternatingCharacters has the following parameter(s):
    
    string s: a string
    Returns
    
    int: the minimum number of deletions required
    Input Format
    
    The first line contains an integer , the number of queries.
    The next  lines each contain a string  to analyze.
    
    Constraints
    
    Each string  will consist only of characters  and .
    Sample Input
    
    5
    AAAA
    BBBBB
    ABABABAB
    BABABA
    AAABBB
    Sample Output
    
    3
    4
    0
    0
    4

---
+++ alternatingCharacters.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    if s.__contains__("A") and s.__contains__("B"):
+        d = 0
+        for x in range(1,len(s)):
+            if s[x-1] == s[x] :
+                d+=1
+        return d
+    else:
+        return len(s)-1
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
